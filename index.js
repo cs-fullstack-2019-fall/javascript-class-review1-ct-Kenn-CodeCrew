ddb1018facdca08e48f925edca3dfbbbe70d1b96
Kenn <kenn+git@code-crew.org>
2019-08-16 11:41:19 -0500

    kenn test

--- index.js
+++ index.js
@@ -3,29 +3,52 @@
 // Problem 1:
 // Put the start of your program in a main function. Create a two player game. Ask the first player to "Enter a number". The second player has to guess the number until they get the right number. After each guess let them know if they should go higher or lower.
 
-var player1Number = prompt("Enter a number for them to guess");
-var player2Guess = prompt("Enter a guess")
-while(player1Number !== player2Guess)
-{
-    if(player2Guess < player1Number)
-    {
-        alert("Guess higher!!!");
-    }
-    else if(player2Guess > player1Number)
-    {
-        alert("Guess lower!!!!");
-    }
-    player2Guess = prompt("Try again")
-}
-
-alert("You win")
+// var player1Number = prompt("Enter a number for them to guess");
+// var player2Guess = prompt("Enter a guess")
+// while(player1Number !== player2Guess)
+// {
+//     if(player2Guess < player1Number)
+//     {
+//         alert("Guess higher!!!");
+//     }
+//     else if(player2Guess > player1Number)
+//     {
+//         alert("Guess lower!!!!");
+//     }
+//     player2Guess = prompt("Try again")
+// }
+//
+// alert("You win")
 
 //
 //     Problem 2:
 // Put the start of your program in a main function. Create a class for a bank account user. Include name, address, and account balance. Create a function that can change the account balance. It WILL NOT be changed if you log it. Create 3 bank users and put them in an array. Print the name and balance of each person in a function.
+class BankAccountUser{
+    constructor(name) {
+        this.name = name;
+    }
+}
 
+function main() {
+    let arrayOfNames = ["Kenn", "Kevin", "Erin"];
+    let arrayOfObjects = [];
 
+    arrayOfNames.forEach(
+        function(eachElementInTheArrayOneAfterTheOther)
+        {
+            let tempUser = new BankAccountUser(eachElementInTheArrayOneAfterTheOther);
+            arrayOfObjects.push(tempUser);
+        }
+    );
 
+    for(let index in arrayOfObjects)
+    {
+        console.log(arrayOfObjects[index].name);
+    }
+    // console.log(arrayOfObjects);
+}
+
+main();
 //
 //     Problem 3:
 // Put the start of your program in a main function. Create a function that can translate five English words into Spanish words. You should be able to call your function from your main function.
